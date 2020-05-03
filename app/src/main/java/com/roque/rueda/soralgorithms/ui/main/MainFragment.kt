86faf7f34d8fa2b8e2ed8a9ee373d91e4261b412
Roque Rueda <skaroque@gmail.com>
2020-05-02 20:47:14 -0500

    Add function to create random numbers

--- app/src/main/java/com/roque/rueda/soralgorithms/ui/main/MainFragment.kt
+++ app/src/main/java/com/roque/rueda/soralgorithms/ui/main/MainFragment.kt
@@ -1,14 +1,12 @@
 package com.roque.rueda.soralgorithms.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.roque.rueda.soralgorithms.R
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.main_fragment) {
 
     companion object {
         fun newInstance() = MainFragment()
@@ -18,10 +16,5 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
     }
 }
--- app/src/main/java/com/roque/rueda/soralgorithms/ui/main/MainViewModel.kt
+++ app/src/main/java/com/roque/rueda/soralgorithms/ui/main/MainViewModel.kt
@@ -1,7 +1,26 @@
 package com.roque.rueda.soralgorithms.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+const val NUMBER_OF_LINES = 500
 
 class MainViewModel : ViewModel() {
 
+    private val _lines = MutableLiveData<IntArray>()
+    val lines: LiveData<IntArray> get() = _lines
+
+    fun calculateRandomArray() {
+        viewModelScope.launch {
+            val randomNumbers = createRandomIntArray()
+            _lines.value = randomNumbers
+        }
+    }
+
+    private fun createRandomIntArray() =
+        IntArray(NUMBER_OF_LINES) { Random.nextInt(NUMBER_OF_LINES) }
 }
